1a81f64b541dbcbc8423d44e05254e8b293497ad
veronica-foltz <vfoltz1520@gmail.com>
2026-06-11 09:14:51 -1000

    Add full CRUD operations for horses

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
@@ -28,6 +28,15 @@ def root():
 def get_horses(db: Session = Depends(get_db)):
     return db.query(models.Horse).all()
 
+@app.get("/horses/{horse_id}")
+def get_horse(horse_id: int, db: Session = Depends(get_db)):
+    horse = db.query(models.Horse).filter(models.Horse.id == horse_id).first()
+
+    if horse is None:
+        raise HTTPException(status_code=404, detail="Horse not found")
+
+    return horse
+
 @app.post("/horses")
 def create_horse(horse: HorseCreate, db: Session = Depends(get_db)):
     db_horse = models.Horse(
@@ -40,3 +49,43 @@ def create_horse(horse: HorseCreate, db: Session = Depends(get_db)):
     db.refresh(db_horse)
 
     return db_horse
+
+@app.put("/horses/{horse_id}")
+def update_horse(
+    horse_id: int,
+    horse: HorseCreate,
+    db: Session = Depends(get_db)
+):
+
+    db_horse = db.query(models.Horse).filter(
+        models.Horse.id == horse_id
+    ).first()
+
+    if db_horse is None:
+        raise HTTPException(status_code=404, detail="Horse not found")
+
+    db_horse.name = horse.name
+    db_horse.breed = horse.breed
+
+    db.commit()
+    db.refresh(db_horse)
+
+    return db_horse
+
+@app.delete("/horses/{horse_id}")
+def delete_horse(
+    horse_id: int,
+    db: Session = Depends(get_db)
+):
+
+    horse = db.query(models.Horse).filter(
+        models.Horse.id == horse_id
+    ).first()
+
+    if horse is None:
+        raise HTTPException(status_code=404, detail="Horse not found")
+
+    db.delete(horse)
+    db.commit()
+
+    return {"message": f"Horse {horse_id} deleted"}
