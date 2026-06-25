5059d2decd9d9ff0123628fa4e526e826b16b62c
veronica-foltz <vfoltz1520@gmail.com>
2026-06-25 09:20:36 -1000

    added schema.py

---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class HorseCreate(BaseModel):
+    name: str
+    breed: str
+
+class TaskCreate(BaseModel):
+    title: str
+    status: str = "Pending"
+    horse_id: int
+    employee_id: int
+
+class TaskUpdate(BaseModel):
+    title: str
+    status: str
+    horse_id: Optional[int] = None
+    employee_id: Optional[int] = None
+
+class EmployeeCreate(BaseModel):
+    name: str
+    role: str
+
+class EmployeeUpdate(BaseModel):
+    name: str
+    role: str
