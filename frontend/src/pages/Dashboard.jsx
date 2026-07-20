27f222919e50ab722e9a0ea4dcffceb5ccf53301
veronica-foltz <vfoltz1520@gmail.com>
2026-07-20 12:48:39 -1000

    Add React Router and dashboard page

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+    return (
+        <div>
+            <h1>Welcome to Stable Ops!</h1>
+            <p>You are successfully logged in.</p>
+        </div>
+    );
+}
+
+export default Dashboard;
