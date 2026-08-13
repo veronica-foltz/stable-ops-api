06ead78f56c58a880c6b8e05f95088d49f55620b
veronica-foltz <vfoltz1520@gmail.com>
2026-08-13 08:56:51 -1000

    Fixed dashboard routing issue

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navigation() {
     return (
         <nav className="navigation">
-            <Link to="/">Dashboard</Link>
+            <Link to="/dashboard">Dashboard</Link>
             <Link to="/horses">Horses</Link>
             <Link to="/employees">Employees</Link>
             <Link to="/tasks">Tasks</Link>
