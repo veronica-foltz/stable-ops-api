f71a1f8ddc9b1fed6c4f246656be39ad89b44fba
veronica-foltz <vfoltz1520@gmail.com>
2026-08-13 08:10:46 -1000

    Added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Dashboard from "./pages/Dashboard";
 
 import Horses from "./pages/Horses";
 
+import Employees from "./pages/Employees";
+import Tasks from "./pages/Tasks";
+
 function App() {
     return (
         <Routes>
