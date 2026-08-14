da2f547a75c722cf05f5ab65897da7f456304d83
veronica-foltz <vfoltz1520@gmail.com>
2026-08-14 09:40:53 -1000

    Configure database for production

--- app/database.py
+++ app/database.py
@@ -1,14 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://veronica@localhost:5433/stable_ops"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://veronica@localhost:5433/stable_ops",
+)
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 
 Base = declarative_base()
